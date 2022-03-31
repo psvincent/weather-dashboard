7cf7e68b57e06e9c1d2cd50f99b30a6ceda68991
psvincent12 <psvincent12@gmail.com>
2022-03-31 03:24:48 -0500

    change repo name

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,31 @@
-var apiKey = '180c8d68d4552c60900fe20d3d097e75'
+// grabbing ids from html and assigning them to variables in js
+var search = document.getElementById("search");
+var cityInput = document.getElementById("city-input");
+var cityName = document.getElementById("current-city-date");
+var temp = document.getElementById("temperature");
+var currentWeather = document.getElementById("forecast");
 
-var city = "London"
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-.then(function(response) {
-    return response.json();
+// Getting initial date setup
+var date = $("#current-date").text(moment().format("MMMM Do YYYY, M:DD:YYYY"));
+
+// Giving my api key a variable so I don't have to copy and paste it in the api link
+var apiKey = '180c8d68d4552c60900fe20d3d097e75';
+
+// Function that calls the API and gets the weather from it
+function getWeather() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`)
+    .then((response) => {
+        return response.json();
+    
+    
 })
-.then(function(data) {
-    console.log(data)
-    return data.coord;
-})
+
+getWeather();
+
+
+// Saving the city entry into local storage
+// search.addEventListener("click", function() {
+//     var searchedCity = cityInput.value;
+
+
+// })
